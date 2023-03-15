ea7687b41904ac8c6ae337633971595f8691f3ea
[ManikandanArunachalam] <[maniuki1@gmail.com]>
2023-03-15 10:34:54 +0530

    deploy

---
+++ src/components/BannerBottom.js
@@ -0,0 +1,25 @@
+function BannerBottom()
+{
+    return(
+        <section className="section border-0 m-0 appear-animation animated fadeIn appear-animation-visible"
+				data-appear-animation="fadeIn" data-appear-animation-delay="1200" style={{ animationDelay: '1200ms' }}>
+				<div className="container">
+					<div className="row align-items-center justify-content-center">
+						<div className="col-sm-9 col-md-12 col-lg-7 text-center text-lg-end mb-4 mb-lg-0">
+							<h2 className="font-weight-bold text-color-primary text-7 line-height-1 mb-1 appear-animation animated fadeInLeftShorterPlus appear-animation-visible"
+								data-appear-animation="fadeInLeftShorterPlus" data-appear-animation-delay="1700"
+								style={{ animationDelay: '1700ms' }}>Kind of work is so Best & Good</h2>
+							<p className="font-weight-bold text-4 mb-0 appear-animation animated fadeInLeftShorterPlus appear-animation-visible"
+								data-appear-animation="fadeInLeftShorterPlus" data-appear-animation-delay="1900"
+								style={{ animationDelay: '1900ms' }}>Make An Enquiry Today With Our Online Form</p>
+						</div>
+						<div className="col-lg-5 text-center text-lg-start ps-lg-4">
+							
+						</div>
+					</div>
+				</div>
+			</section>
+    )
+}
+
+export default BannerBottom;
